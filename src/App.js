73fcf2776af8557d17be6f358f002fde518653fa
hyebinyu <hyebinyu1110@gmail.com>
2022-07-04 23:48:49 +0900

    practice with todoapp-5

--- src/App.js
+++ src/App.js
@@ -3,26 +3,22 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "todolist 외우기",
-      checked: true,
-    },
-    {
-      id: 2,
-      text: "샤워하기, 공부에 집중하기",
-      checked: true,
-    },
-    {
-      id: 3,
-      text: "선물 주기",
+function createBulkTodos(){
+  const array = [];
+  for(let i=1; i<=2500; i++){
+    array.push({
+      id: i,
+      text: `할일 ${i}`,
       checked: false,
-    }
-  ])
+    });
+  }
+  return array;
+}
 
-  const nextId = useRef(4);
+const App = () => {
+  const [todos, setTodos] = useState(createBulkTodos);
+
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(
     text => {
