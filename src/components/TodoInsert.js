6426c42d27c361dd9168ff7270128129c410816c
hyebin yu <hyebinyu1110@gmail.com>
2022-07-04 16:55:44 +0900

    practice with todoapp-3

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -2,13 +2,13 @@ import './TodoInsert.scss';
 import { MdAdd } from 'react-icons/md';
 import { useState, useCallback } from 'react';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
     const [value, setValue] = useState('');
 
-    const onChange = (e) => {
+    const onChange = useCallback(e => {
         setValue(e.target.value);
-        console.log(value);
-    }
+    }, []);
+
     return (
         <form className="TodoInsert">
             <input placeholder="할 일을 입력하세요" value={value} onChange={onChange} />
